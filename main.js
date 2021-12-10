b95c628dcd8c05db879dabbaad546196187a88b1
Liammouhali <mouhaliliam7@gmail.com>
2021-12-10 20:09:16 +0100

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+let last_section = undefined;
+
+function new_section(section){
+    if(last_section !== undefined){
+        document.getElementById(last_section).style.display="none";
+    }
+    document.getElementById(section).style.display="flex";
+
+    last_section = section;
+}
+
+new_section('main_menu');
+
+function add_server(name,players,slots){
+    document.querySelector('#server_list .content').innerHTML += "<div><div><h1>"+ name +"</h1></div><h2>"+ players +"/"+ slots +"</h2></div>"
+}
